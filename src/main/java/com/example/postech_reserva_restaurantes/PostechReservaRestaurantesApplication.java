1bd8e8ba502481acd0d7bd3f491693e5e6adfa02
Ana Silva <anacxx@live.com>
2025-03-04 17:07:49 -0300

    Commit inicial com estrutura de pastas.

---
+++ src/main/java/com/example/postech_reserva_restaurantes/PostechReservaRestaurantesApplication.java
@@ -0,0 +1,13 @@
+package com.example.postech_reserva_restaurantes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PostechReservaRestaurantesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PostechReservaRestaurantesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/postech_reserva_restaurantes/PostechTechReservaRestaurantesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.postech_reserva_restaurantes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PostechReservaRestaurantesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
