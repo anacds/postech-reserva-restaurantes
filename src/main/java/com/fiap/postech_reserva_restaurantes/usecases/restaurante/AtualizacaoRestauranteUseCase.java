d580031664fcc9e6a2510ae138c103ab911ad61f
Ana Silva <anacxx@live.com>
2025-03-23 14:59:37 -0300

    Use Case AtualizacaoRestauranteUseCase

--- src/main/java/com/fiap/postech_reserva_restaurantes/usecases/restaurante/AtualizacaoRestauranteUseCase.java
+++ src/main/java/com/fiap/postech_reserva_restaurantes/usecases/restaurante/AtualizacaoRestauranteUseCase.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class AtualizacaoRestauranteUseCase {
@@ -19,7 +20,51 @@ public class AtualizacaoRestauranteUseCase {
     public RestauranteGateway restauranteGateway;
 
     public RestauranteEntity atualizarRestaurante(RestauranteDTO restauranteDTO) {
-        return null;
+        if (restauranteDTO == null) {
+            throw new IllegalArgumentException("Dados do restaurante não podem ser nulos.");
+        }
+
+        Optional<RestauranteEntity> restauranteExistente = restauranteGateway.buscarRestaurantePorId(restauranteDTO.id());
+        if (restauranteExistente.isEmpty()) {
+            throw new IllegalArgumentException("Restaurante não encontrado para atualização.");
+        }
+
+        CNPJEntity cnpj = new CNPJEntity(restauranteDTO.cnpj());
+
+        EnderecoEntity endereco = new EnderecoEntity(
+                restauranteDTO.endereco().rua(),
+                restauranteDTO.endereco().numero(),
+                restauranteDTO.endereco().bairro(),
+                restauranteDTO.endereco().cidade(),
+                restauranteDTO.endereco().estado(),
+                restauranteDTO.endereco().cep(),
+                restauranteDTO.endereco().complemento()
+        );
+
+        List<HorarioFuncionamentoEntity> horariosFuncionamento = new ArrayList<>();
+        for (var horarioDTO : restauranteDTO.horariosFuncionamento()) {
+            HorarioFuncionamentoEntity horario = new HorarioFuncionamentoEntity(
+                    horarioDTO.diaSemana(),
+                    horarioDTO.horarioAbertura(),
+                    horarioDTO.horarioFechamento()
+            );
+            horariosFuncionamento.add(horario);
+        }
+
+        RestauranteEntity restauranteAtualizado = new RestauranteEntity(
+                restauranteDTO.nome(),
+                cnpj,
+                endereco,
+                restauranteExistente.get().getNota(),
+                restauranteExistente.get().getComentarios(),
+                restauranteDTO.tipoCozinha(),
+                horariosFuncionamento,
+                restauranteDTO.capacidade()
+        );
+
+        validarDadosRestaurante(restauranteAtualizado);
+
+        return restauranteGateway.atualizarRestaurante(restauranteAtualizado);
     }
 
     private static void validarDadosRestaurante(RestauranteEntity restaurante) {
